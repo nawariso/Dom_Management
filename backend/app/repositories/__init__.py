2671f9942f9aed5efab6829ed135e2bb029e9671
nawariso <102949919+nawariso@users.noreply.github.com>
2025-12-19 00:56:04 +0700

    Add repository helpers and coverage tests

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,25 @@
+from .base import Repository
+from .core import (
+    BuildingRepository,
+    DormRepository,
+    FloorRepository,
+    InvoiceRepository,
+    PaymentRepository,
+    RoomRepository,
+    SubscriptionRepository,
+    TenantRepository,
+    UsageRecordRepository,
+)
+
+__all__ = [
+    "Repository",
+    "BuildingRepository",
+    "DormRepository",
+    "FloorRepository",
+    "InvoiceRepository",
+    "PaymentRepository",
+    "RoomRepository",
+    "SubscriptionRepository",
+    "TenantRepository",
+    "UsageRecordRepository",
+]
